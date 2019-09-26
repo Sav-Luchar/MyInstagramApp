e9c0e1c3ec7854ad4cd932006e21d4379c4f371b
Lulu-Luchar <chillhouette@gmail.com>
2019-09-25 22:50:22 -0700

    Created subroutes with dummy data

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const app = express();
 
 //DB Config
@@ -12,6 +15,11 @@ mongoose
     .catch(err=> console.log(err));
 
 //First route
-app.get('/',(req,res) => res.send('The Empress first Instagram App'));
+app.get("/",(req,res) => res.send("The Empress first Instagram App"));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = 4000;
 app.listen(port,() => console.log (`Server successfully running on port ${port}`));
