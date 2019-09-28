adaaf60633c4621beb4335755cbec9b0ff4b2ac5
Lulu-Luchar <chillhouette@gmail.com>
2019-09-27 22:14:30 -0700

    Encrypted pw with bcrypt and finished /register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/User')
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require ('bcryptjs');
 
 //@route POST api/users/register
 //@desc Register a user
@@ -14,16 +16,49 @@ router.post('/register',(req,res)=> {
                 email: 'Email already exists'
             })
         } else {
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password:req.body.password
+            const avatar = gravatar.url(req.body.email, {
+                s:'200',
+                r: 'pg',
+                d: 'mm'
             });
+            const newUser = new User ({
+                name: req.body.name,
+                email:req.body.email,
+                avatar: avatar,
+                password: req.body.password
+            });
+            bcrypt.genSalt(10, (err,salt)=> {
+                if (err) throw err;
+                bcrypt.hash(newUser.password, salt, (err,hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then( user => res.json(user))
+                    .catch(err => console.log(err));
+                })
+            })
         }
     })
     .catch(err => console.log(err));
 })
 
+//@route POST api/users/login
+//@desc Login a user/ return JWT token
+//@access Public 
+router.post('/login',(req,res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    //Find user by email
+    User.findOne({email})
+    .then(user => {
+        if(!user) {
+            return res.status(404).json({
+                email: 'User is missing in action'
+            });
+        }
+    })
+    .catch(err => console.log(err));
+})
 
 module.exports= router;
